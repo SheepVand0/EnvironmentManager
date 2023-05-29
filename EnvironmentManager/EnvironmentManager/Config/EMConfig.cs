14175df187fe1cea2dc483aadb92bfb22c536cab
SheepVand <66167164+SheepVand0@users.noreply.github.com>
2023-05-29 10:36:39 +0200

    Init

---
+++ EnvironmentManager/EnvironmentManager/Config/EMConfig.cs
@@ -0,0 +1,21 @@
+﻿using CP_SDK.Config;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvironmentManager.Config
+{
+    internal class EMConfig : JsonConfig<EMConfig>
+    {
+
+        public override string GetRelativePath()
+            => $"{CP_SDK.ChatPlexSDK.ProductName}/EnvironmentManager/Config";
+
+        [JsonProperty] internal bool IsEnabled = true;
+         
+    
+    }
+}
